edcbe2aec159cc7323ebb6a0c2685315439f64ad
Mustafa YILMAZ <101803552+mustpre@users.noreply.github.com>
2022-05-20 16:04:40 +0300

    Add files via upload

---
+++ lib/text_output.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TextOuput extends StatelessWidget {
+  final String mainText;
+
+  TextOuput(this.mainText);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text(mainText);
+  }
+}
